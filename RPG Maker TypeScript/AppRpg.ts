4bb5c3477de7902b295c0f09c4b4016a0b4fdca7
Rodrigo Daros <rodrigo.conspena@gmail.com>
2016-11-14 17:40:38 -0200

    RPG Maker:
    - Adição do projeto em typescript para fazer a parte dos jogadores.

---
+++ RPG Maker TypeScript/AppRpg.ts
@@ -0,0 +1,31 @@
+﻿/// <reference path="../Web.TypeScript/AppWebBase.ts"/>
+
+module Rpg
+{
+    // #region Importações
+
+    import AppWebBase = NetZ_Web.AppWebBase;
+
+    // #endregion Importações
+
+    // #region Enumerados
+    // #endregion Enumerados
+
+    export class AppRpg extends AppWebBase
+    {
+        // #region Constantes
+        // #endregion Constantes
+
+        // #region Atributos
+        // #endregion Atributos
+
+        // #region Construtores
+        // #endregion Construtores
+
+        // #region Métodos
+        // #endregion Métodos
+
+        // #region Eventos
+        // #endregion Eventos
+    }
+}
